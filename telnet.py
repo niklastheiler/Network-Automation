ed2c52fbb986fc0ce1a48f99024df263638b1cf4
Niklas Theiler <Niklas.Theiler@outlook.de>
2022-04-07 16:41:57 +0200

    Updated the script

--- telnet.py
+++ telnet.py
@@ -2,18 +2,16 @@ import getpass
 import telnetlib
 
 HOST = "localhost"
-user = input("Enter your remote account: ")
+user = input("Username: ")
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 
-tn.read_until(b"login: ")
+tn.read_until(b"Username: ")
 tn.write(user.encode('ascii') + b"\n")
+
 if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
-
-tn.write(b"ls\n")
-tn.write(b"exit\n")
 
 print(tn.read_all().decode('ascii'))
